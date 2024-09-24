0ef429135ce809befcc83724a6d65f17f46863d9
karla <karlaacunainca@gmail.com>
2024-09-24 16:01:54 -0500

    url de botón regreso a catalogo

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -36,7 +36,7 @@ export default function ProductDetailPage() {
 
   return (
     <> <div className="product-detail-container">
-       <Link to="/catalog" className="back-to-catalog-button">Regresar a catálogo</Link>
+       <Link to="/catalogo" className="back-to-catalog-button">Regresar a catálogo</Link>
        <br />
       {product && (
         <div className="product-detail-container2">
