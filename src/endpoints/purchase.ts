694bc69c5a9db7d19f1bb848a5877efc3f87e7e7
eperez <eperez@nic.cl>
2024-11-12 21:49:27 -0300

    Se vuleve a versión anterior de carro de compra para levantar la imagen de docker compose

--- src/endpoints/purchase.ts
+++ src/endpoints/purchase.ts
@@ -1,66 +1,36 @@
 import { CartItem } from "../interfaces/CartItem";
 import { CartState } from "../states/cartSlice";
 
-// Función para crear un item en el carrito
-export const createCartItem = async (cartData: CartItem) => {
+export const finalizePurchaseRequest = async (cartData: CartItem[]) => {
+    const cartState: CartState = {
+        idUsuario:1,
+        productos: cartData
+    };
+
+    
     try {
         const response = await fetch('http://localhost:8080/carro-compras', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'accept': '*/*'
+
             },
-            body: JSON.stringify(cartData),
+            body: JSON.stringify(cartState),
         });
-
-        if (!response.ok) throw new Error('Error al crear item en el carrito');
         
+        console.log('Respuesta del servidor:', response); // Verifica la respuesta
+        
+        const statusCode = response.status;
+        if (!response.ok) {
+            throw new Error('Error en la finalización de la compra');
+        }
+
         const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error al crear item:', error);
-        throw error;
-    }
-};
-
-// Función para editar un item en el carrito
-export const editCartItem = async (cartData: CartItem) => {
-    try {
-        const response = await fetch(`http://localhost:8080/carro-compras/${cartData.id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': '*/*'
-            },
-            body: JSON.stringify(cartData),
-        });
-
-        if (!response.ok) throw new Error('Error al editar item en el carrito');
-        
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error al editar item:', error);
-        throw error;
-    }
-};
-
-// Función para eliminar un item en el carrito
-export const deleteCartItem = async (cartItemId: string) => {
-    try {
-        const response = await fetch(`http://localhost:8080/carro-compras/${cartItemId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': '*/*'
-            }
-        });
-
-        if (!response.ok) throw new Error('Error al eliminar item del carrito');
-        
-        return { message: 'Item eliminado correctamente' };
-    } catch (error) {
-        console.error('Error al eliminar item:', error);
-        throw error;
+        return {data, statusCode}; // Retorna la respuesta del servidor si es exitosa.
+    } 
+    catch (error) {
+        console.error('Error en la finalización de la compra:', error);
+        throw error; // Lanza el error para manejarlo en el componente.
     }
 };
